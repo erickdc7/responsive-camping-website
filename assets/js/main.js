c15176eefca7167885d0aaa91b09105927213cd3
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-10-22 13:32:14 -0500

    gsap animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,10 @@ const bgHeader = () => {
 window.addEventListener('scroll', bgHeader)
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__img-2', 1.2, { opacity: 0, y: 200, delay: .1 })
+gsap.from('.home__img-3', 1.2, { opacity: 0, y: 200, delay: .5 })
+gsap.from('.home__data', 1.2, { opacity: 0, y: -60, delay: 1 })
+gsap.from('.home__bird-1', 1.2, { opacity: 0, x: -80, delay: 1.1 })
+gsap.from('.home__bird-2', 1.2, { opacity: 0, x: 80, delay: 1.2 })
+gsap.from('.home__img-1', 1.2, { opacity: 0, y: 200, delay: 1.2 })
+gsap.from('.home__img-4', 1.2, { opacity: 0, x: 200, delay: 1.3 })
